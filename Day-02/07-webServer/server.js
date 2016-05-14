61416a7b166bb8e85ce9e21bf1bcee4ae6ad56a1
tkmagesh <tkmagesh77@gmail.com>
2016-05-14 12:12:36 +0530

    assignment

--- Day-02/07-webServer/server.js
+++ Day-02/07-webServer/server.js
@@ -1,5 +1,12 @@
 var http = require('http');
 var server = http.createServer(function(req, res){
+	/*
+	req.url -> resources requested
+	look at file system api to know how to check if file exists
+	if (file not found)
+		res.statusCode = 404;
+		res.end()
+	*/
 	res.write('<h1>Welcome to node.js</h1>');
 	res.end();
 });
