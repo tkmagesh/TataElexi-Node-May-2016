967704036bf0745e393512f13fc5e64c878630c0
tkmagesh <tkmagesh77@gmail.com>
2016-05-14 12:52:51 +0530

    Static web server - solution

--- Day-02/07-webServer/server.js
+++ Day-02/07-webServer/server.js
@@ -1,14 +1,24 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
 var server = http.createServer(function(req, res){
-	/*
-	req.url -> resources requested
-	look at file system api to know how to check if file exists
-	if (file not found)
-		res.statusCode = 404;
-		res.end()
-	*/
-	res.write('<h1>Welcome to node.js</h1>');
-	res.end();
+	console.log(req.method, ' - ', req.url);
+	req.url = req.url === '/' ? '/index.html' : req.url;
+	
+	var resource = path.join(__dirname, req.url);
+	fs.stat(resource, function(err, stats){
+		if (err){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		if (stats.isFile()){
+			var fileContents = fs.readFileSync(resource, {encoding : 'utf8'});
+			res.write(fileContents);
+			res.end();
+		}
+	});
 });
 server.listen(8080);
 console.log('server listening on port 8080..!');
